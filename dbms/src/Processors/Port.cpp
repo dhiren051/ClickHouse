20b632e454d6e2cf3b518ae643766b48fd628bb9
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-02-18 19:36:07 +0300

    Added Chunkclass to move columns between ports.

--- dbms/src/Processors/Port.cpp
+++ dbms/src/Processors/Port.cpp
@@ -1,7 +1,87 @@
 #include <Processors/Port.h>
+#include <IO/WriteHelpers.h>
 
 namespace DB
 {
+
+Chunk::Chunk(DB::Columns columns_, UInt64 num_rows_) : columns(std::move(columns_)), num_rows(num_rows_)
+{
+    checkNumRowsIsConsistent();
+}
+
+Chunk::Chunk(Columns columns_, UInt64 num_rows_, ChunkInfoPtr chunk_info_)
+    : columns(std::move(columns_)), num_rows(num_rows_), chunk_info(std::move(chunk_info_))
+{
+    checkNumRowsIsConsistent();
+}
+
+Chunk::Chunk(Chunk && other) noexcept
+    : columns(std::move(other.columns))
+    , num_rows(other.num_rows)
+    , chunk_info(std::move(other.chunk_info))
+{
+    other.num_rows = 0;
+}
+
+Chunk & Chunk::operator=(Chunk && other) noexcept
+{
+    columns = std::move(other.columns);
+    chunk_info = std::move(other.chunk_info);
+    other.num_rows = 0;
+    return *this;
+}
+
+void Chunk::setColumns(Columns columns_, UInt64 num_rows_)
+{
+    columns = std::move(columns_);
+    num_rows = num_rows_;
+    checkNumRowsIsConsistent();
+}
+
+void Chunk::setColumns(MutableColumns columns_, UInt64 num_rows_)
+{
+    size_t num_columns = columns_.size();
+    columns.resize(num_columns);
+    for (size_t i = 0; i < num_columns; ++i)
+        columns[i] = std::move(columns_[i]);
+
+    num_rows = num_rows_;
+    checkNumRowsIsConsistent();
+}
+
+void Chunk::checkNumRowsIsConsistent()
+{
+    for (auto & column : columns)
+        if (column->size() != num_rows)
+            throw Exception("Invalid number of rows in Chunk column " + column->getName()+ ": expected " +
+                            toString(num_rows) + ", got " + toString(column->size()), ErrorCodes::LOGICAL_ERROR);
+}
+
+MutableColumns Chunk::mutateColumns()
+{
+    size_t num_columns = columns.size();
+    MutableColumns mut_columns(num_columns);
+    for (size_t i = 0; i < num_columns; ++i)
+        mut_columns[i] = (*std::move(columns[i])).mutate();
+
+    columns.clear();
+    num_rows = 0;
+
+    return mut_columns;
+}
+
+Columns Chunk::detachColumns()
+{
+    num_rows = 0;
+    return std::move(columns);
+}
+
+void Chunk::clear()
+{
+    num_rows = 0;
+    columns.clear();
+    chunk_info.reset();
+}
 
 void connect(OutputPort & output, InputPort & input)
 {
